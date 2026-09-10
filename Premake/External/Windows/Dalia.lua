2520e9f68f9c0d399028b5681c07eb0589ecf9f4
Alexander Cao <XANDERMAN80@hotmail.com>
2026-09-10 15:32:01 +0200

    Compiling dalia uses the correct directory

--- Premake/External/Windows/Dalia.lua
+++ Premake/External/Windows/Dalia.lua
@@ -11,13 +11,13 @@ project "Dalia"
 
     filter "configurations:release"
         prebuildcommands{
-            "{MKDIR} %{prj.objdir}",
-            "cmake -S " .. moduleDirectory .. " -B %{prj.objdir} -DCMAKE_INSTALL_PREFIX=%{prj.targetdir} -DCMAKE_MSVC_RUNTIME_LIBRARY='MultiThreaded'",
-            "cmake --build %{prj.objdir} --config %{cfg.buildcfg} --target install",
+            "{MKDIR} %{path.getabsolute(prj.objdir)}",
+            "cmake -S " .. moduleDirectory .. " -B %{path.getabsolute(prj.objdir)} -DCMAKE_INSTALL_PREFIX=%{prj.targetdir} -DCMAKE_MSVC_RUNTIME_LIBRARY='MultiThreaded' -DDALIA_BUILD_STUDIO=OFF -DDALIA_BUILD_SANDBOX=OFF",
+            "cmake --build %{path.getabsolute(prj.objdir)} --config %{cfg.buildcfg} --target install",
         }
     filter "configurations:debug"
         prebuildcommands{
-            "{MKDIR} %{prj.objdir}",
-            "cmake -S " .. moduleDirectory .. " -B %{prj.objdir} -DCMAKE_INSTALL_PREFIX=%{prj.targetdir} -DCMAKE_MSVC_RUNTIME_LIBRARY='MultiThreadedDebug'",
-            "cmake --build %{prj.objdir} --config %{cfg.buildcfg} --target install",
+            "{MKDIR} %{path.getabsolute(prj.objdir)}",
+            "cmake -S " .. moduleDirectory .. " -B %{path.getabsolute(prj.objdir)} -DCMAKE_INSTALL_PREFIX=%{prj.targetdir} -DCMAKE_MSVC_RUNTIME_LIBRARY='MultiThreadedDebug' -DDALIA_BUILD_STUDIO=OFF -DDALIA_BUILD_SANDBOX=OFF",
+            "cmake --build %{path.getabsolute(prj.objdir)} --config %{cfg.buildcfg} --target install",
         }
