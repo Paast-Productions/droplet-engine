8bcf469d0bd562b9702ebad41aed175b63746022
Alexander Cao <XANDERMAN80@hotmail.com>
2026-09-10 11:08:27 +0200

    Added gli to premake files

--- premake5.lua
+++ premake5.lua
@@ -44,9 +44,11 @@ include "Premake/External/ImGui"
 if _TARGET_OS == 'windows' then
     include "Premake/External/Windows/GoogleTest"
     include "Premake/External/Windows/Assimp"
+    include "Premake/External/Windows/Gli"
 end
 
 if _TARGET_OS == 'linux' then
     include "Premake/External/Linux/GoogleTest"
     include "Premake/External/Linux/Assimp"
+    include "Premake/External/Linux/Gli"
 end
