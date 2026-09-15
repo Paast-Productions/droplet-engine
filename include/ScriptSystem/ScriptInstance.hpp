38f119ff847b4de0b6622a1d96fe3c05e165bd6e
ponlin02 <ponlin.02@gmail.com>
2026-09-15 14:58:52 +0200

    Added ScripInstance class
    Class may change this is a starter for the script system

---
+++ include/ScriptSystem/ScriptInstance.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../../External/Sol2/include/sol/sol.hpp"
+#include "string"
+
+class ScriptInstance
+{
+public:
+	ScriptInstance(sol::state_view p_luaState, const std::string& p_scriptPath);
+	~ScriptInstance();
+
+	bool Load();
+
+	void onStart();
+	void onUpdate(float deltatime);
+
+private:
+	sol::state_view m_luaState;
+	sol::environment m_environment;
+
+	std::string m_sriptPath;
+
+	sol::protected_function m_onStart;
+	sol::protected_function m_onUpdate;
+
+	bool m_loaded = false;
+};
