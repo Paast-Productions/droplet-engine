a49f3f58984d808a4b03211aa86cf9cfd1f51dbb
GuptaW <hwaltersson03@gmail.com>
2026-09-15 15:07:26 +0200

    Script Manager and system startup

---
+++ include/ScriptSystem/ScriptSystem.cpp
@@ -0,0 +1,13 @@
+#include "ScriptSystem.hpp"
+
+void ScriptSystem::Initiliaze()
+{
+}
+
+void ScriptSystem::Shutdown()
+{
+}
+
+void ScriptSystem::Update(float deltaTime)
+{
+}
--- include/ScriptSystem/ScriptSystem.hpp
+++ include/ScriptSystem/ScriptSystem.hpp
@@ -1,6 +1,16 @@
 #pragma once
-
+#include "ScriptManager.hpp"
+#include "LuaStateHandler.hpp"
 class ScriptSystem
 {
+public:
+	void Initiliaze();
+	void Shutdown();
+	void Update(float deltaTime);
+	void SetScriptDirectory(char* filePath);
+
+private:
+	ScriptManager m_scriptManager;
+	
 
 };
