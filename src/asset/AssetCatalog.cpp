36521ddf2049ebd66f0d94d20f4f76fce528f84a
Arvid Wahlgren <salliban03@gmail.com>
2026-09-16 13:57:56 +0200

    Implement AssetCatalog

---
+++ src/asset/AssetCatalog.cpp
@@ -0,0 +1,31 @@
+﻿#include "AssetCatalog.h"
+
+namespace engine
+{
+    void AssetCatalog::ScanDirectory(const std::filesystem::path& p_directory)
+    {
+        for (const auto& entry : std::filesystem::recursive_directory_iterator(p_directory))
+        {
+            if (entry.path().extension() == ".meta")
+            {
+                // Read json formatted meta file
+                // Extract guid
+                // Derive asset path from file name
+                // Add to m_pathToGuid
+            }
+        }
+    }
+
+    GUID AssetCatalog::GetGUID(const std::string& p_path)
+    {
+        auto it = m_pathToGuid.find(p_path);
+        if (it != m_pathToGuid.end())
+        {
+            return it->second;
+        }
+    
+        return 0; // Invalid guid
+    }
+}
+
+
---
+++ src/asset/AssetCatalog.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <filesystem>
+#include <json/json.hpp>
+
+namespace engine
+{
+    using GUID = uint64_t; // temporary alias
+
+    /// @brief Maps asset path strings to GUIDs
+    class AssetCatalog
+    {
+    private:
+        std::unordered_map<std::string, GUID> m_pathToGuid; // Maps paths to GUIDs
+    
+    public:
+        /// @brief Scans the provided directory recursively for .meta files to populate its internal map of GUIDs.
+        /// @param p_directory The directory to be scanned.
+        void ScanDirectory(const std::filesystem::path& p_directory);
+
+        /// @brief Retrieves the globally unique identifier for an asset.
+        /// @param p_path The path to the asset.
+        /// @return The globally unique identifier.
+        GUID GetGUID(const std::string& p_path);
+    };
+}
