cba5b2d7d38a095537badae6f93dbac5cd27cbb4
SkyH1ghDev <contact@skyh1gh.dev>
2026-09-17 15:23:21 +0200

    Slang: Intermediary Push

--- Samples/Vulkan/HelloTriangle.cpp
+++ Samples/Vulkan/HelloTriangle.cpp
@@ -1,5 +1,11 @@
 ﻿#include <print>
+#include <filesystem>
+#include <array>
+#include <slang/slang.h>
+#include <slang/slang-com-ptr.h>
 #include "Renderer.hpp"
+
+void CompileShader(std::filesystem::path p_path);
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
@@ -40,4 +46,45 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 	}
     
     return 0;
+}
+
+void CompileShader(std::filesystem::path p_path)
+{
+	Slang::ComPtr<slang::IGlobalSession> globalSession {};
+	slang::createGlobalSession(globalSession.writeRef());
+	
+	slang::TargetDesc targetDesc 
+	{
+		.format = SLANG_SPIRV,
+		.profile = globalSession->findProfile("spirv_1_5")
+	};
+	
+	std::array<slang::PreprocessorMacroDesc, 2> preprocessorMacroDescs
+	{
+		{
+			{ "BIAS_VALUE", "1138" },
+			{ "OTHER_MACRO", "float" }
+		}
+	};
+	
+	slang::SessionDesc sessionDesc 
+	{
+		.targets = &targetDesc,
+		.targetCount = 1,
+		.defaultMatrixLayoutMode = SLANG_MATRIX_LAYOUT_COLUMN_MAJOR
+	};
+	
+	Slang::ComPtr<slang::ISession> session {};
+	globalSession->createSession(sessionDesc, session.writeRef());
+	
+	Slang::ComPtr<slang::IBlob> diagnostics {};
+	Slang::ComPtr<slang::IModule> module
+	(
+		session->loadModule("shader", diagnostics.writeRef())
+	);
+	
+	if (!module)
+	{
+		std::print("Diagnostics: {0}", static_cast<const char*>(diagnostics->getBufferPointer()));
+	}
 }
