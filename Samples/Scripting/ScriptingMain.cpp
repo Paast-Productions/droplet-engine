962337df03e643dd737c96d279d78fcb388e7d42
GuptaW <hwaltersson03@gmail.com>
2026-09-17 16:20:21 +0200

    test node

--- Samples/Scripting/ScriptingMain.cpp
+++ Samples/Scripting/ScriptingMain.cpp
@@ -1,8 +1,10 @@
 ﻿#include <print>
 #include "ScriptSystem/ScriptSystem.hpp"
+#include "ScriptSystem/TestNode.hpp"
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
+    TestNode node;
     ScriptSystem scriptSystem;
     std::print("Hello World\n");
     // Gupta learns git fork
