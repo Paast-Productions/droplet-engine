c513b92cff17a0e48fb0846f2e4305dfa3277733
muqtada <muqtada14@gmail.com>
2026-09-17 10:09:48 +0200

    i added thread and json.

--- include/Logger.cpp
+++ include/Logger.cpp
@@ -1,72 +1,43 @@
-#include <chrono>
-#include <iostream>
+#include <format>
 #include "Logger.hpp"
-#include "format"
-#include "thread"
-
-//namespace Engine
-//{
-
-logger::~logger()
-{
-	writingToFile.clear();
-	writingToFile.close();
-}
-
-static void WriteLogEntry(const auto &local_time,
-		std::ofstream &writingToFile,
-		const std::string &msg,
-		std::string_view status)
-{
-	std::string totalmsg = std::format(
-		"{{\n"
-		"  \"level\": \"{}\",\n"
-		"  \"timestamp\": \"{:%Y-%m-%d %H:%M:%S}\",\n"
-		"  \"message\": \"{}\"\n"
-		"}},\n",
-		status,
-		local_time,
-		msg
-	);
-	writingToFile << totalmsg;
-}
 
 void logger::Log(LOG_TYPE type, const std::string &msg)
 {
-	auto now = std::chrono::system_clock::now();
+    std::scoped_lock<std::mutex> lock(m_g_i_mutex);
+    std::string s_status{};
+    switch (type)
+    {
+    case Error:   s_status = "Error"; break;
+    case Warning: s_status = "Warning"; break;
+    case Info:    s_status = "Info"; break;
+    case Debug:   s_status = "Debug"; break;
+    }
 
-	if (!writingToFile.is_open())
-		writingToFile.open("Logger.txt", std::ios_base::app);
+    auto now = std::chrono::system_clock::now();
+    auto second = std::chrono::time_point_cast<std::chrono::seconds>(now);
+    auto local_time = std::chrono::zoned_time{ std::chrono::current_zone(), second };
 
-	auto second = std::chrono::time_point_cast<std::chrono::seconds>(now);
-	auto local_time = std::chrono::zoned_time{ std::chrono::current_zone(), second };
+    std::string s_timeStr = std::format("{:%Y-%m-%d %H:%M:%S}", local_time);
+    std::string s_threadStr = std::format("{}", std::this_thread::get_id());
 
-	switch (type)
-	{
-	case Error:
-		WriteLogEntry(local_time, writingToFile, msg, "Error");
-		break;
-	case Warning:
-		WriteLogEntry(local_time, writingToFile, msg, "Warning");
-		break;
-	case Info:
-		WriteLogEntry(local_time, writingToFile, msg, "Info");
-		break;
-	case Debug:
-		WriteLogEntry(local_time, writingToFile, msg, "Debug ");
-		break;
-	}
+    json j = nlohmann::json{
+        {"Level",     s_status},
+        {"Timestamp", s_timeStr},
+        {"Message",   msg},
+        {"ThreadID",  s_threadStr}
+    };
+
+    std::ofstream writingToJsonFile("logger.json", std::ios_base::app);
+    if (writingToJsonFile.is_open())
+    {
+        writingToJsonFile << j.dump(4) << ",\n";
+    }
+    writingToJsonFile.close();
 }
 
-void logger::clearLog()
+void logger::ClearLog()
 {
-	if (writingToFile.is_open())
-	{
-		writingToFile.close();
-	}
-
-	writingToFile.open("Logger.txt", std::ios_base::out | std::ios_base::trunc);
-	writingToFile.close();
-
-	writingToFile.open("Logger.txt", std::ios_base::app);
+	std::scoped_lock<std::mutex> lock(m_g_i_mutex);
+    std::ofstream writingToJsonFile("Logger.json", std::ios_base::out | std::ios_base::trunc);
+    writingToJsonFile.close();
 }
--- include/Logger.hpp
+++ include/Logger.hpp
@@ -1,34 +1,64 @@
 #pragma once
-
-//namespace Engine
-//{
 #include <fstream>
+#include <mutex>
+#include <chrono>
+#include <thread>
+#include <string>
+#include <json/json.hpp>
+
+using json = nlohmann::json;
 
 
-#define LOG_ERROR(msg) ::logger::Log(::logger::LOG_TYPE::Error, msg)
+/**
+ * @class logger
+ * @brief Thread-safe logger class for writing formatted JSON logs.
+ *
+ * Provides thread-safe logging functionality to capture timestamped
+ * messages and thread IDs to a JSON file.
+ */
+
+
+/**
+* @brief Convenience macro to log an error message.
+* @param msg The message string to log.
+*/
+#define LOG_ERROR(msg)   ::logger::Log(::logger::LOG_TYPE::Error, msg)
 #define LOG_WARNING(msg) ::logger::Log(::logger::LOG_TYPE::Warning, msg)
-#define LOG_INFO(msg) ::logger::Log(::logger::LOG_TYPE::Info, msg)
-#define LOG_DEBUG(msg) ::logger::Log(::logger::LOG_TYPE::Debug, msg)
+#define LOG_INFO(msg)    ::logger::Log(::logger::LOG_TYPE::Info, msg)
+#define LOG_DEBUG(msg)   ::logger::Log(::logger::LOG_TYPE::Debug, msg)
+
 
 class logger
 {
 public:
-	inline static std::ofstream writingToFile{};
-public:
+	/**
+	 * @enum LOG_TYPE
+	 * @brief Defines the log severity level selected for an entry.
+	 */
 	enum LOG_TYPE
 	{
-		Error,
-		Warning,
-		Info,
-		Debug
+		Error,   ///< Critical error events that require attention.
+		Warning, ///< Warning events indicating potential issues.
+		Info,    ///< Informational messages about system operation.
+		Debug    ///< Detailed information for debugging purposes.
 	};
 
-public:
 	logger() = default;
-	~logger();
-public:
-	static void Log(LOG_TYPE type, const std::string& msg);
-	static void clearLog();
-};
 
-//}
+	~logger() = default;
+
+	/**
+	 * @brief Main logging function, typically invoked via logging macros.
+	 * Serializes and appends a log entry to the target JSON file.
+	 *
+	 * @param type log entry (e.g., Error, Warning, Info, Debug).
+	 * @param msg The message text to be logged.
+	 */
+	static void Log(LOG_TYPE type, const std::string &msg);
+	/**
+	 * @brief Clears all content from the log file and close the file.
+	 */
+	static void ClearLog();
+private:
+	inline static std::mutex m_g_i_mutex;
+};
