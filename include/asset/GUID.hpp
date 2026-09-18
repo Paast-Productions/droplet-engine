ec7629807738f49c119597e5f97869a48da5c9b5
Arvid Wahlgren <salliban03@gmail.com>
2026-09-18 13:29:53 +0200

    Split GUID.hpp into .hpp and .cpp

--- include/asset/GUID.hpp
+++ include/asset/GUID.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstdint>
-#include <random>
 
 namespace Droplet
 {
@@ -10,19 +9,11 @@ namespace Droplet
     
     namespace GuidUtils
     {
-        static GUID Generate()
-        {
-            thread_local std::random_device rd;
-            thread_local std::mt19937_64 gen(rd());
-            thread_local std::uniform_int_distribution<uint64_t> dis;
-        
-            GUID guid = dis(gen);
-            while (guid == C_INVALID_GUID) // 0 invalid guid
-            {
-                guid = dis(gen);
-            }
-        
-            return guid;
-        }
+        /// @return True if the GUID is valid (non-zero), otherwise false.
+        bool IsValid(GUID guid);
+
+        /// @brief Generates a new 64-bit GUID.
+        /// @return The newly generated GUID.
+        static GUID Generate();
     }
 }
---
+++ src/asset/GUID.cpp
@@ -0,0 +1,26 @@
+#include "asset/GUID.hpp"
+
+#include <random>
+
+namespace Droplet
+{
+    bool GuidUtils::IsValid(GUID guid)
+    {
+        return guid != C_INVALID_GUID;
+    }
+
+    GUID GuidUtils::Generate()
+    {
+        thread_local std::random_device rd;
+        thread_local std::mt19937_64 gen(rd());
+        thread_local std::uniform_int_distribution<uint64_t> dis;
+        
+        GUID guid = dis(gen);
+        while (guid == C_INVALID_GUID) // 0 invalid guid
+        {
+            guid = dis(gen);
+        }
+        
+        return guid;
+    }
+}
