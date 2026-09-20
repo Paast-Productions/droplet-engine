e1fdb2513fe54a34e28fb62b40c36388ccf7346c
Edvin Roth <99540868+ExR-dev@users.noreply.github.com>
2026-09-20 13:20:08 +0200

    Basic definition of texture resources

--- include/asset/Resource.hpp
+++ include/asset/Resource.hpp
@@ -34,36 +34,185 @@ namespace Droplet
 	/// @brief Abstract class for all texture resources.
 	class TextureResource : public IResource
 	{
-		// TODO
-
 	public:
+		enum class TextureFormat
+		{
+			Unknown,
 
-	protected:
+			// Color formats
+			R8_Unorm,
+			R8_Snorm,
+			R8_Uscaled,
+			R8_Sscaled,
+			R8_Uint,
+			R8_Sint,
+
+			R16_Unorm,
+			R16_Snorm,
+			R16_Uscaled,
+			R16_Sscaled,
+			R16_Uint,
+			R16_Sint,
+
+			R32_Unorm,
+			R32_Snorm,
+			R32_Uscaled,
+			R32_Sscaled,
+			R32_Uint,
+			R32_Sint,
+
+			RGB8_Unorm,
+			RGB8_Snorm,
+			RGB8_Uscaled,
+			RGB8_Sscaled,
+			RGB8_Uint,
+			RGB8_Sint,
+
+			RGB16_Unorm,
+			RGB16_Snorm,
+			RGB16_Uscaled,
+			RGB16_Sscaled,
+			RGB16_Uint,
+			RGB16_Sint,
+
+			RGB32_Unorm,
+			RGB32_Snorm,
+			RGB32_Uscaled,
+			RGB32_Sscaled,
+			RGB32_Uint,
+			RGB32_Sint,
+
+			RGBA8_Unorm,
+			RGBA8_Snorm,
+			RGBA8_Uscaled,
+			RGBA8_Sscaled,
+			RGBA8_Uint,
+			RGBA8_Sint,
+			   
+			RGBA16_Unorm,
+			RGBA16_Snorm,
+			RGBA16_Uscaled,
+			RGBA16_Sscaled,
+			RGBA16_Uint,
+			RGBA16_Sint,
+			   
+			RGBA32_Unorm,
+			RGBA32_Snorm,
+			RGBA32_Uscaled,
+			RGBA32_Sscaled,
+			RGBA32_Uint,
+			RGBA32_Sint,
+
+			// Depth formats
+			D24S8,
+			D32,
+
+			// Compressed formats
+			BC1,
+			BC2,
+			BC3,
+			BC4,
+			BC5,
+			BC6H,
+			BC7,
+
+			// Add more formats as needed...
+		};
+
+		/// @brief Sets the format of the texture data.
+		/// @param p_format The format of the texture data.
+		/// @param p_bytesPerPixel The number of bytes per pixel in the texture data.
+		void SetFormat(TextureFormat p_format, int p_bytesPerPixel) { m_format = p_format; m_bytesPerPixel = p_bytesPerPixel; }
+
+		/// @brief Sets the number of mipmap levels in the texture.
+		/// @param p_mipLevels The number of mipmap levels in the texture.
+		void SetMipLevels(int p_mipLevels) { m_mipLevels = p_mipLevels; }
+
+		/// @brief Sets the pixel data for the texture.
+		/// @param p_pixelData The pixel data for the texture.
+		void SetPixelData(const std::vector<std::byte> &p_pixelData) { m_pixelData = p_pixelData; }
+
+		/// @brief Gets the format of the texture data.
+		/// @return The format of the texture data.
+		[[nodiscard]] TextureFormat GetFormat() const { return m_format; }
+
+		/// @brief Gets the number of bytes per pixel in the texture data.
+		/// @return The number of bytes per pixel in the texture data.
+		[[nodiscard]] int GetBytesPerPixel() const { return m_bytesPerPixel; }
+
+		/// @brief Gets the number of mipmap levels in the texture.
+		/// @return The number of mipmap levels in the texture.
+		[[nodiscard]] int GetMipLevels() const { return m_mipLevels; }
+
+		/// @brief Gets the pixel data for the texture.
+		/// @return A vector of bytes representing the pixel data for the texture.
+		[[nodiscard]] const std::vector<std::byte> &GetPixelData() const { return m_pixelData; }
 
 	private:
+		TextureFormat	m_format = TextureFormat::Unknown;	// The format of the texture data
+		int				m_mipLevels = 1;					// The number of mipmap levels in the texture
+		int				m_bytesPerPixel = 0;				// The number of bytes per pixel in the texture data (invalid for compressed formats)
 
+		std::vector<std::byte> m_pixelData{};				// Pixel data for the texture
 	};
 
 	/// @brief Class for 2D texture resources.
 	class Texture2DResource : public TextureResource
 	{
-		// TODO
-
 	public:
+		/// @brief Sets the width and height of the texture.
+		/// @param p_width The width of the texture.
+		/// @param p_height The height of the texture.
+		void SetDimensions(std::uint32_t p_width, std::uint32_t p_height)
+		{
+			m_width = p_width;
+			m_height = p_height;
+		}
+
+		/// @brief Gets the width of the texture.
+		/// @return The width of the texture.
+		[[nodiscard]] std::uint32_t GetWidth() const { return m_width; }
+
+		/// @brief Gets the height of the texture.
+		/// @return The height of the texture.
+		[[nodiscard]] std::uint32_t GetHeight() const { return m_height; }
 
 	private:
-
+		std::uint32_t m_width = 0;		// The width of the texture
+		std::uint32_t m_height = 0;		// The height of the texture
 	};
 
 	/// @brief Class for 3D texture resources.
 	class Texture3DResource : public TextureResource
 	{
-		// TODO
-
 	public:
+		/// @brief Sets the width, height, and depth of the texture.
+		/// @param p_width The width of the texture.
+		/// @param p_height The height of the texture.
+		/// @param p_depth The depth of the texture.
+		void SetDimensions(std::uint32_t p_width, std::uint32_t p_height, std::uint32_t p_depth)
+		{
+			m_width = p_width;
+			m_height = p_height;
+			m_depth = p_depth;
+		}
+
+		/// @brief Gets the width of the texture.
+		/// @return The width of the texture.
+		[[nodiscard]] std::uint32_t GetWidth() const { return m_width; }
+
+		/// @brief Gets the height of the texture.
+		/// @return The height of the texture.
+		[[nodiscard]] std::uint32_t GetHeight() const { return m_height; }
+
+		/// @brief Gets the depth of the texture.
+		/// @return The depth of the texture.
+		[[nodiscard]] std::uint32_t GetDepth() const { return m_depth; }
 
 	private:
-
+		std::uint32_t m_width = 0;		// The width of the texture
+		std::uint32_t m_height = 0;		// The height of the texture
+		std::uint32_t m_depth = 0;		// The depth of the texture
 	};
 
 	/// @brief Class for mesh resources.
@@ -231,9 +380,21 @@ namespace Droplet
 		// TODO
 
 	public:
+		enum class ShaderType
+		{
+			Vertex,
+			Fragment,
+			Geometry,
+			Compute,
+			TessellationControl,
+			TessellationEvaluation,
+			Mesh,
+			Task,
+		};
 
 	private:
 
+		ShaderType m_shaderType = ShaderType::Vertex;
 	};
 
 	/// @brief Class for material resources.
