f463712c7e6ed33488ce8802b5041bba121fcf89
ponlin02 <ponlin.02@gmail.com>
2026-09-21 16:40:23 +0200

    Merge conflict stuff

--- include/ScriptSystem/ScriptSystem.hpp
+++ include/ScriptSystem/ScriptSystem.hpp
@@ -12,13 +12,18 @@ public:
 
 	void Start();
 	void Update(float p_deltaTime);
-	void Shutdown();
 
 	bool LoadScript(const std::string& p_scriptFile);
 	bool UnloadScript(const std::string& p_scriptFile);
 
+	void ActivateScript(TestNode* p_scriptComponent);
+	void DeactivateScript(TestNode* p_scriptComponent);
+
 	ScriptInstance* CreateScript(TestNode* testNode, const std::string& p_scriptFile);
 	void DestroyScript(ScriptInstance* p_scriptInstance);
+
+	template<typename... Args>
+	sol::protected_function_result Call(TestNode* p_scriptComponent, std::string_view p_funcitonName, Args&&... p_args);
 
 	//void SetScriptDirectory(char* filePath);
 
@@ -26,3 +31,9 @@ private:
 	LuaStateHandler m_luaStateHandler;
 	ScriptManager m_scriptManager;
 };
+
+template<typename... Args>
+inline sol::protected_function_result ScriptSystem::Call(TestNode* p_scriptComponent, std::string_view p_functionName, Args&&... p_args)
+{
+	return m_scriptManager.Call(p_scriptComponent, p_functionName, std::forward<Args>(p_args)...);
+}
