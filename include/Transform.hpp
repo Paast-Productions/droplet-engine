8577186f9690aae9471c515159d51f0683b282e0
Edvin Roth <99540868+ExR-dev@users.noreply.github.com>
2026-09-21 13:15:36 +0200

    Add Transform class declaration

---
+++ include/Transform.hpp
@@ -0,0 +1,149 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/quaternion.hpp>
+#include <memory>
+
+class Node; // Forward declaration of Node class
+
+namespace Droplet
+{
+	/// @brief A class that represents a 3D transformation, including position, rotation, and scale.
+	class Transform
+	{
+	public:
+		enum class Space
+		{
+			Local,
+			World
+		};
+
+		/// @brief Construct a new Transform object.
+		/// @param owner A weak pointer to the Node that owns this Transform.
+		Transform(std::weak_ptr<Node> owner);
+
+		~Transform();
+
+		// ================================ Getters ================================
+
+		/// @brief Get the position of the transform in the specified space.
+		/// @param space The space in which to get the position. Defaults to local space.
+		/// @return The position of the transform in the specified space.
+		[[nodiscard]] const glm::vec3 &GetPosition(Space space = Space::Local) const;
+
+		/// @brief Get the rotation of the transform in the specified space.
+		/// @param space The space in which to get the rotation. Defaults to local space.
+		/// @return The rotation of the transform in the specified space.
+		[[nodiscard]] const glm::quat &GetRotation(Space space = Space::Local) const;
+
+		/// @brief Get the Euler angles of the transform in the specified space.
+		/// @param space The space in which to get the Euler angles. Defaults to local space.
+		/// @return The Euler angles of the transform in the specified space.
+		[[nodiscard]] glm::vec3 GetEuler(Space space = Space::Local) const;
+
+		/// @brief Get the local scale of the transform.
+		/// @return The local scale of the transform.
+		[[nodiscard]] const glm::vec3 &GetScale() const;
+
+		/// @brief Get the transformation matrix of the transform in the specified space.
+		/// @param space The space in which to get the matrix. Defaults to local space.
+		/// @return The transformation matrix of the transform in the specified space.
+		[[nodiscard]] const glm::mat4 &GetMatrix(Space space = Space::Local) const;
+
+		/// @brief Check if the transform is dirty (i.e., if it has been modified since the last update).
+		/// @return True if the transform is dirty, false otherwise.
+		[[nodiscard]] bool IsDirty() const;
+
+		/// @brief Get the up direction of the transform in the specified space.
+		/// @param space The space in which to get the up direction. Defaults to local space.
+		/// @return The up direction of the transform in the specified space.
+		[[nodiscard]] glm::vec3 GetUp(Space space = Space::Local) const;
+
+		/// @brief Get the right direction of the transform in the specified space.
+		/// @param space The space in which to get the right direction. Defaults to local space.
+		/// @return The right direction of the transform in the specified space.
+		[[nodiscard]] glm::vec3 GetRight(Space space = Space::Local) const;
+
+		/// @brief Get the forward direction of the transform in the specified space.
+		/// @param space The space in which to get the forward direction. Defaults to local space.
+		///	@return The forward direction of the transform in the specified space.
+		[[nodiscard]] glm::vec3 GetForward(Space space = Space::Local) const;
+
+		// ================================ Setters ================================
+
+		/// @brief Set the position of the transform in the specified space.
+		/// @param position The new position to set.
+		/// @param space The space in which to set the position. Defaults to local space.
+		void SetPosition(const glm::vec3 &position, Space space = Space::Local);
+
+		/// @brief Set the rotation of the transform in the specified space.
+		/// @param rotation The new rotation to set.
+		/// @param space The space in which to set the rotation. Defaults to local space.
+		void SetRotation(const glm::quat &rotation, Space space = Space::Local);
+
+		/// @brief Set the Euler angles of the transform in the specified space.
+		/// @param eulerAngles The new Euler angles to set.
+		/// @param space The space in which to set the Euler angles. Defaults to local space.
+		void SetEuler(const glm::vec3 &eulerAngles, Space space = Space::Local);
+
+		/// @brief Set the local scale of the transform.
+		/// @param scale The new scale to set.
+		void SetScale(const glm::vec3 &scale);
+
+		/// @brief Set the transformation matrix of the transform in the specified space.
+		/// @param matrix The transformation matrix to set.
+		/// @param space The space in which to set the matrix. Defaults to local space.
+		void SetMatrix(const glm::mat4 &matrix, Space space = Space::Local);
+
+		/// @brief Mark the transform as dirty, indicating that it has been modified and needs to be updated.
+		void MakeDirty();
+
+		// ================================ Utility ================================
+
+		/// @brief Update the world matrix of the transform based on its local matrix and the parent's world matrix.
+		/// Sets the transform as clean after updating. Will update recursively if the parent transform is dirty.
+		void RecalculateWorldMatrix();
+
+		/// @brief Move the transform by a specified delta in the specified space.
+		/// @param delta The delta by which to move the transform.
+		/// @param space The space in which to perform the move operation. Defaults to local space.
+		void Move(const glm::vec3 &delta, Space space = Space::Local);
+
+		/// @brief Rotate the transform by a specified delta in the specified space.
+		/// @param delta The delta by which to rotate the transform.
+		/// @param space The space in which to perform the rotate operation. Defaults to local space.
+		void Rotate(const glm::quat &delta, Space space = Space::Local);
+
+		/// @brief Rotate the transform by a specified delta in Euler angles in the specified space.
+		/// @param delta The delta by which to rotate the transform in Euler angles.
+		/// @param space The space in which to perform the rotate operation. Defaults to local space.
+		void RotateEuler(const glm::vec3 &delta, Space space = Space::Local);
+
+		/// @brief Add a scale to the transform in local space.
+		/// @param delta The delta to add to the current scale of the transform.
+		void AddScale(const glm::vec3 &delta);
+
+		/// @brief Rotate the transform around a specified axis by a specified angle in the specified space.
+		/// @param angle The angle in radians by which to rotate the transform.
+		/// @param axis The axis around which to rotate the transform.
+		/// @param space The space in which to perform the rotate operation. Defaults to local space.
+		void RotateAxis(float angle, const glm::vec3 &axis, Space space = Space::Local);
+
+		/// @brief Make the transform look at a target position.
+		/// @param target The target position to look at.
+		/// @param up The up direction for the transform. Defaults to (0, 1, 0).
+		/// @param space The space in which to perform the look-at operation. Defaults to local space.
+		void LookAt(const glm::vec3 &target, const glm::vec3 &up = glm::vec3(0.f, 1.f, 0.f), Space space = Space::Local);
+
+	private:
+		std::weak_ptr<Node> m_owner{};
+		bool				m_isDirty{ true };
+
+		glm::vec3			m_position{ 0.f, 0.f, 0.f };
+		glm::quat			m_rotation{ 0.f, 0.f, 0.f, 1.f };
+		glm::vec3			m_scale{ 1.f, 1.f, 1.f };
+
+		glm::mat4			m_worldMatrix{ 1.f };
+		glm::mat4			m_localMatrix{ 1.f };
+	};
+}
