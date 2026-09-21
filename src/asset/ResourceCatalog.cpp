d25492b4ced10af98923c1176f3e6387e3ba3c06
Arvid Wahlgren <salliban03@gmail.com>
2026-09-21 11:43:32 +0200

    RegisterAsset pseudo-implementation

--- src/asset/ResourceCatalog.cpp
+++ src/asset/ResourceCatalog.cpp
@@ -34,6 +34,38 @@ namespace Droplet
     
         return false;
     }
+
+    bool ResourceCatalog::GetCachedMetaDataForAsset(const std::string &p_assetPath,
+        std::vector<MetaEntry> &p_metaData)
+    {
+        p_metaData.clear();
+        auto it = m_assetToResourcesMap.find(p_assetPath);
+        if (it != m_assetToResourcesMap.end())
+        {
+            for (GUID guid : it->second)
+            {
+                auto metaDataIt = m_guidToDataMap.find(guid);
+                if (metaDataIt != m_guidToDataMap.end())
+                {
+                    p_metaData.push_back(metaDataIt->second);
+                }
+            }
+            
+            return true;
+        }
+        
+        return false; // Asset is not registered
+    }
+
+    void ResourceCatalog::RegisterMetaEntry(const std::string &p_assetPath, const MetaEntry &p_metaEntry)
+    {
+        m_guidToDataMap[p_metaEntry.guid] = p_metaEntry;
+        
+        auto& guidList = m_assetToResourcesMap[p_assetPath];
+        if (std::find(guidList.begin(), guidList.end(), p_metaEntry.guid) == guidList.end())
+        {
+            guidList.push_back(p_metaEntry.guid);
+        } 
     }
 }
 
--- src/asset/ResourceManager.cpp
+++ src/asset/ResourceManager.cpp
@@ -1,5 +1,7 @@
 ﻿#include "Asset/ResourceManager.hpp"
 
+#include "meta/MetaSerializer.hpp"
+
 // #include "IResource"
 
 namespace Droplet
@@ -20,6 +22,43 @@ namespace Droplet
         }
         
         m_isInitialized = true;
+    }
+
+    void ResourceManager::RegisterAsset(const std::filesystem::path &p_assetPath)
+    {
+        std::vector<MetaEntry> metaEntry;
+        
+        std::filesystem::path metaPath = p_assetPath.string() + ".meta";
+        if (std::filesystem::exists(metaPath))
+        {
+            std::string assetPathStr = p_assetPath.string();
+            std::vector<MetaEntry> cachedMetaData;
+            
+            m_catalog.GetCachedMetaDataForAsset(assetPathStr, cachedMetaData);
+            
+            // Thread pool dispatch here with lambda[this, p_assetPath, assetPathStr, cachedMetaData]
+            // --- Async Worker Thread ---
+            std::vector<MetaEntry> discoveredResources;
+            
+            // Run assimp/whatever to detect resources in asset file
+            
+            // Dispatch back to main thread with lambda[this, assetPathStr, resourceList, cachedMetaData]
+            std::vector<MetaEntry> mergedMetaData;
+            
+            // Use private function to compare cached metadata to resource list and append/remove bodies here
+            
+            // Overwrite .meta file with result (dispatch I/O thread to perform this task)
+            // --- Async I/O Thread --- 
+            std::filesystem::path metaPath = assetPathStr + ".meta";
+            MetaSerializer::Write(metaPath, mergedMetaData);
+            
+            // --- Main Thread ---
+            // Update internal catalog
+            for (const auto &entry : mergedMetaData)
+            {
+                m_catalog.RegisterMetaEntry(assetPathStr, entry);
+            }
+        }
     }
 
     void ResourceManager::AddRef(GUID p_guid)
