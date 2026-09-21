a6ac3f49df12d307367e976513ba208421e8940e
SkyH1ghDev <contact@skyh1gh.dev>
2026-09-21 13:40:28 +0200

    Temp Stash

---
+++ include/Algorithm/Hash/Utilities.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <cstddef>
+
+namespace Droplet::Algorithm::Hash
+{
+    class Utilities
+    {
+    public:
+        
+        template<class T>
+        static void Combine(const T &p_value, std::size_t &p_result);
+    };
+}
+
