fa93012547b895f397d41210d1a1a7a1676266d7
Linus Holmström <linus.holmstrom@hotmail.com>
2026-09-21 17:30:26 +0200

    Started work on command pool class.
    Working on creating wrappers for command pools, the command pool
    wrapper is in early stages now.

---
+++ src/Graphics/VK/CommandPool.cpp
@@ -0,0 +1,8 @@
+﻿#include "CommandPool.hpp"
+
+Droplet::Graphics::VK::CommandPool::CommandPool(const vk::raii::Device &p_device, const uint32_t p_queueFamilyIndex,
+	const vk::CommandPoolCreateFlagBits p_flags)
+{
+	vk::CommandPoolCreateInfo createInfo = {.flags = p_flags, .queueFamilyIndex = p_queueFamilyIndex};
+	m_commandPool = vk::raii::CommandPool(p_device, createInfo);
+}
