05e859bd729ea95b047c9ee09f69b99722e6e9cb
Linus Holmström <linus.holmstrom@hotmail.com>
2026-09-21 17:30:26 +0200

    Started work on command pool class.
    Working on creating wrappers for command pools, the command pool
    wrapper is in early stages now.

---
+++ include/Graphics/VK/CommandPool.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <vulkan/vulkan_raii.hpp>
+
+namespace Droplet::Graphics::VK
+{
+	class CommandPool
+	{
+	public:
+		CommandPool() = delete;
+		CommandPool(const vk::raii::Device &p_device, const uint32_t p_queueFamilyIndex, const vk::CommandPoolCreateFlagBits p_flags = vk::CommandPoolCreateFlagBits::eResetCommandBuffer);
+		
+	private:
+		vk::raii::CommandPool m_commandPool = nullptr;
+	};
+}
