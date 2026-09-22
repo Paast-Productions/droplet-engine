dff8afca3be788d4ab2ba1ca10f09487909bc6d9
ponlin02 <ponlin.02@gmail.com>
2026-09-22 16:10:10 +0200

    Scripting system tests made

--- Test/src/ScriptSystem/UT_ScriptSystem.cpp
+++ Test/src/ScriptSystem/UT_ScriptSystem.cpp
@@ -5,25 +5,68 @@
 TEST(ScriptSystem, LoadScript)
 {
 	ScriptSystem system;
+	system.SetScriptPath("../src/TestScripts");
 	EXPECT_TRUE(system.LoadScript("testScript.lua"));
 }
 
 TEST(ScriptSystem, UnloadScript)
 {
 	ScriptSystem system;
+	system.SetScriptPath("../src/TestScripts");
 	system.LoadScript("testScript.lua");
 	EXPECT_TRUE(system.UnloadScript("testScript.lua"));
+}
+
+TEST(ScriptSystem, CreateScript)
+{
+	ScriptSystem system;
+	system.SetScriptPath("../src/TestScripts");
+	TestNode testNode;
+
+	ScriptInstance* instance = system.CreateScript(&testNode, "testScript.lua");
+	EXPECT_NE(instance, nullptr);
+}
+
+TEST(ScriptSystem, DestroyScript)
+{
+	ScriptSystem system;
+	system.SetScriptPath("../src/TestScripts");
+	TestNode testNode;
+
+	ScriptInstance* instance = system.CreateScript(&testNode, "testScript.lua");
+	
+	//Function DestroyScript should destroy instance not an entire script
+	//system.DestroyScript(instance);
+
+	EXPECT_EQ(instance, nullptr);
 }
 
 TEST(ScriptSystem, ActivateScript)
 {
 	ScriptSystem system;
+	system.SetScriptPath("../src/TestScripts");
 	TestNode testNode;
-	system.CreateScript(&testNode, "testScript.lua");
+
+	system.CreateScript(&testNode, "bruh.lua");
 	system.ActivateScript(&testNode);
-	//Will activate the update loop for the node
+	system.Update(1);
+	float result = system.Call(&testNode, "getDT");
+
+	EXPECT_EQ(result, 1);
 }
 
 TEST(ScriptSystem, DeactivateScript)
 {
+	ScriptSystem system;
+	system.SetScriptPath("../src/TestScripts");
+	TestNode testNode;
+
+	system.CreateScript(&testNode, "bruh.lua");
+	system.ActivateScript(&testNode);
+	system.Update(1);
+	system.DeactivateScript(&testNode);
+	system.Update(1);
+	float result = system.Call(&testNode, "getDT");
+
+	EXPECT_EQ(result, 1);
 }
