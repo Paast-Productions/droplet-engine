6d122c9a36273d63922a2f932355193b940a9c24
HalvEmil <145006109+HalvEmil@users.noreply.github.com>
2026-09-22 12:19:25 +0200

    Fixed param comments in header

--- include/asset/TextureLoader.hpp
+++ include/asset/TextureLoader.hpp
@@ -12,18 +12,18 @@ namespace Droplet::ResourceLoader
 {
 	/// @brief Texture resource that holds a gli texture. Can load dds and
 	/// KTX files but also convert PNG files to KTX format 
-	class TextureLoader /* : public IResource */
+	class TextureLoader
 	{
 	public:
 
 		/// @return loaded gli texture 
-		/// @param string of textures path in explorer
+		/// @param p_path string of textures path in explorer
 		/// @throws the invalid path
 		gli::texture Load(const std::string &p_path);
 
 		/// @return True if PNG succesfully converted to KTX
-		/// @param string of textures path in explorer
-		/// @param string of new path to texture in explorer
+		/// @param p_inputPath string of textures path in explorer
+		/// @param p_outputPath string of new path to texture in explorer
 		/// @throws the invalid path
 		bool ConvertPNGToKTX(const std::string &p_inputPath, const std::string &p_outputPath);
 
