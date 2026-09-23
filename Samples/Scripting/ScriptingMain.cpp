e09acd6c65ed628b094f93f20abd9aa145a728f8
GuptaW <hwaltersson03@gmail.com>
2026-09-23 08:24:51 +0200

    nb

--- Samples/Scripting/ScriptingMain.cpp
+++ Samples/Scripting/ScriptingMain.cpp
@@ -12,23 +12,13 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
         &testNode,
        "testScript.lua"
     ); 
-    
     scriptSystem.CreateScript(&guptaNode, "guptaTestScript.lua");
-    
-    scriptSystem.Start();
     scriptSystem.Call(&guptaNode, "backflip");
-    auto var = scriptSystem.Call(&guptaNode, "fishflip", 5, 2,3);
+    scriptSystem.ActivateScript(&guptaNode);
+    scriptSystem.Call(&guptaNode, "backflip");
 
-    scriptSystem.ActivateScript(&testNode);
-    for (int i = 0; i < 3; ++i)
-    {
-        scriptSystem.Update(1.0f);
-    }
-    scriptSystem.DeactivateScript(&testNode);
-    std::cout << testNode.getX() << '\n';
-    std::cout << guptaNode.getX() << '\n';
+    scriptSystem.DeactivateScript(&guptaNode);
+    scriptSystem.Call(&guptaNode, "backflip");
 
-    scriptSystem.DeactivateScript(&testNode);
-    std::cout << testNode.getX() << '\n';
     return 0;
 } 
