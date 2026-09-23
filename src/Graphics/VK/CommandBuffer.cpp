4fa81d253e6bbc69d918bfc85a32238139e36350
Linus Holmström <linus.holmstrom@hotmail.com>
2026-09-23 17:08:42 +0200

    Working on improved single-time command buffers.
    When finished, it should be possible to invoke an arbitrary command
    on a command buffer created for that command only, which then is
    destroyed.

--- src/Graphics/VK/CommandBuffer.cpp
+++ src/Graphics/VK/CommandBuffer.cpp
@@ -5,6 +5,23 @@ vk::raii::CommandBuffer& Droplet::Graphics::VK::CommandBuffer::Get()
 	return m_commandBuffer;
 }
 
+void Droplet::Graphics::VK::CommandBuffer::Begin(vk::CommandBufferUsageFlagBits p_flags)
+{
+	vk::CommandBufferBeginInfo beginInfo { .flags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit};
+	m_commandBuffer.begin(beginInfo);
+}
+
+void Droplet::Graphics::VK::CommandBuffer::End()
+{
+	m_commandBuffer.end();
+}
+
+void Droplet::Graphics::VK::CommandBuffer::CopyBuffer(vk::Buffer p_src, vk::Buffer p_dst,
+	vk::DeviceSize p_size)
+{
+	m_commandBuffer.copyBuffer(p_src, p_dst, vk::BufferCopy{.size = p_size});
+}
+
 Droplet::Graphics::VK::CommandBuffer::CommandBuffer(const vk::raii::Device &p_device,
                                                     const vk::raii::CommandPool &p_pool, const vk::CommandBufferLevel p_level)
 {
