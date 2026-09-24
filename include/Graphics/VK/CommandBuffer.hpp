ddcee9cf631c89dbfbd2cadf23174eef28fe0458
Linus Holmström <linus.holmstrom@hotmail.com>
2026-09-24 15:27:31 +0200

    Updated doxygen comments in CommandPool and CommandBuffer.
    Some doxygen comments were missing. These have now been added,
    and some parts of existing ones were updated.

--- include/Graphics/VK/CommandBuffer.hpp
+++ include/Graphics/VK/CommandBuffer.hpp
@@ -5,20 +5,36 @@
 
 namespace Droplet::Graphics::VK
 {
+	/// @brief CommandBuffer class to abstract creation and usage of Vulkan CommandBuffers.
 	class CommandBuffer
 	{
 	public:
+		/// @brief Get the Vulkan RAII CommandBuffer associated with the instance. 
 		[[nodiscard]] vk::raii::CommandBuffer& Get();
-		void Begin(vk::CommandBufferUsageFlagBits p_flags);
-		void End();
 		
+		/// @brief Set the command buffer to start recording commands.
+		/// @param p_flags Specify the usage behavior of the command buffer.
+		void Begin(vk::CommandBufferUsageFlagBits p_flags);
+		
+		/// @brief Complete the recording of the command buffer.
+		void End();
+
+		/// @brief Issue command to copy data between two buffers.
+		/// @param p_src The source buffer.
+		/// @param p_dst The destination buffer.
+		/// @param p_size The number of bytes to copy.
+		/// @note Buffers are *not* the same as command buffers. The command buffer is only a middle-hand to copy data.
 		void CopyBuffer(vk::Buffer p_src, vk::Buffer p_dst, vk::DeviceSize p_size);
 		
 	private:
 		friend class CommandPool;
 		
 		CommandBuffer() = delete;
-		
+
+		/// @brief Initialize a CommandBuffer instance.
+		/// @param p_device The device which owns the command pool.
+		/// @param p_pool The command pool to allocate the buffer from.
+		/// @param p_level The command buffer level.
 		CommandBuffer(
 			const vk::raii::Device &p_device,
 			const vk::raii::CommandPool &p_pool,
--- include/Graphics/VK/CommandPool.hpp
+++ include/Graphics/VK/CommandPool.hpp
@@ -9,53 +9,53 @@
 
 namespace Droplet::Graphics::VK
 {
-	// TODO: Improve this doxygen 
-	/// @brief Information about a command buffer
+	/// @brief Information which can be used to find a specific command buffer in the pool.
 	struct CommandBufferId
 	{
 		std::size_t Index;
 	};
 	
+	/// @brief CommandPool class to abstract creation and usage of Vulkan CommandPools.
 	class CommandPool
 	{
 	public:
-		/// @brief Deleted constructor
+		/// @brief Deleted constructor.
 		CommandPool() = delete;
 		
-		/// @brief Deleted constructor
+		/// @brief Deleted constructor.
 		CommandPool(const CommandPool&) = delete;
 		
-		/// @brief Deleted constructor
+		/// @brief Deleted constructor.
 		CommandPool& operator=(const CommandPool&) = delete;
 		
-		/// @brief CommandPool constructor
-		/// @param p_device RAII pointer to the Vulkan Device
-		/// @param p_queueFamilyIndex Index to a queue family which the command buffers should be submitted to
-		/// @param p_flags Flags to create the command pool with
+		/// @brief CommandPool constructor.
+		/// @param p_device RAII pointer to the Vulkan Device.
+		/// @param p_queueFamilyIndex Index to a queue family which the command buffers should be submitted to.
+		/// @param p_flags Flags to create the command pool with.
 		CommandPool(const vk::raii::Device &p_device, const uint32_t p_queueFamilyIndex,
 			const vk::CommandPoolCreateFlags p_flags);
 		
-		/// @brief Create a CommandBuffer in the pool
-		/// @param p_level Level of the command buffer to allocate
-		/// @returns CommandBufferId struct
+		/// @brief Create a CommandBuffer in the pool.
+		/// @param p_level Level of the command buffer to allocate.
+		/// @returns CommandBufferId struct.
 		[[nodiscard]] CommandBufferId Allocate(const vk::CommandBufferLevel p_level = vk::CommandBufferLevel::ePrimary);
 		
-		/// @brief Create multiple CommandBuffers in the pool
-		/// @param p_level Level of the command buffers to allocate
-		/// @param p_count Number of buffers to allocate
-		/// @returns Vector with CommandBufferId structs
+		/// @brief Create multiple CommandBuffers in the pool.
+		/// @param p_level Level of the command buffers to allocate.
+		/// @param p_count Number of buffers to allocate.
+		/// @returns Vector with CommandBufferId structs.
 		[[nodiscard]] std::vector<CommandBufferId> Allocate(uint32_t p_count, vk::CommandBufferLevel p_level = vk::CommandBufferLevel::ePrimary);
 		
-		/// @brief Getter-function for a command buffer matching a descriptor
-		/// @param p_id Struct containing command buffer index in command pool
-		/// @returns Reference to command buffer
+		/// @brief Getter-function for a command buffer matching a descriptor.
+		/// @param p_id Struct containing command buffer index in command pool.
+		/// @returns Reference to command buffer.
 		[[nodiscard]] CommandBuffer& GetBuffer(const CommandBufferId p_id);
 		
 		/// @brief Submit commands to be immediately submitted to the supplied queue.
 		/// 
 		/// @tparam RecordFunction Callable type which accepts a CommandBuffer reference.
 		/// 
-		/// @param p_queue The queue to submit the command to
+		/// @param p_queue The queue to submit the command to.
 		/// @param p_recordFunction Function to be invoked while the command buffer is recorded.
 		/// 
 		/// @pre The queue represented by p_queue must be compatible with the queue family used by this pool.
