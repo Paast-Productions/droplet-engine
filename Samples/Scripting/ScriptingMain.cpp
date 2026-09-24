d0f9f53e56e6d43203c8da00150627ebe0acaa07
Toth <olivertoth.in@gmail.com>
2026-09-24 09:12:52 +0200

    scriptsystem with compoment works

--- Samples/Scripting/ScriptingMain.cpp
+++ Samples/Scripting/ScriptingMain.cpp
@@ -126,11 +126,10 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
     // ==================================================
     // Deactivate / unload
     // ==================================================
-    //ScriptSystem scriptSystem;
 
     player->AddComponent<ScriptComponent>("testScript.lua");
 
-    // A scriptComponent is what talks to the script system
+    ScriptSystem::Get().Start();
 
     return 0;
 } 
